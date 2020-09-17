df102598536a97fd75839ccaa23296819cb4be52
Telychko Vitalii <newme1989.5@gmail.com>
2020-09-17 12:27:13 +0300

    added prepush hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello </h1>
+      <h1>Hello</h1>
+      <Home />
     </div>
   );
 }
